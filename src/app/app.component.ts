737e5e492bfa78089b3f52c1dbf430f1a57d4619
SaraiReyes <idalia_sarai@hotmail.com>
2021-05-06 16:39:49 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'quizz';
+  listadoEstudiantes: any[] = [
+    {nombre: "Luis Sanchez", estado: "Irregular"},
+    {nombre: "Ana Perez", estado: "Regular"},
+    {nombre: "Martin Orozco", estado: "Irregular"},
+    {nombre: "juan Sarmiento", estado: "Regular"},
+    {nombre: "Benito Ortiz", estado: "Regular"},
+  ]
+  /***event binding */
+  texto= 'Este es un ejemplo de event binding'
+  textop= ''
+
+  /**interpolación */
+  nombre= 'Sara'
+  /***property bindins */
+  textoplacehler='Escriba algo aquí'
+  deshabilitado=true
+  /**interpolación */
+  constructor() {
+    setInterval(() => this.nombre = 'invitado', 3000);
+    setInterval(() => this.nombre = 'Bievenido', 3000);
+    setInterval(() => this.deshabilitado = false, 3000);
+
+  }
+  getsuma(uno: number ,dos: number){
+    return uno+ dos
+  }
+  cambiarTexto():void {
+    this.texto='Este texto ha cambiado'
+  }
+  mostrar=true;
+  toggle():void {
+    this.mostrar=!this.mostrar
+  }
 }
